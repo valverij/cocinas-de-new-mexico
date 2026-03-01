91ab33d0c2aa7a651cb62380a4c9bff8f06dfbdf
valverij <valverij@gmail.com>
2026-02-28 20:09:04 -0500

    moved glossary

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,6 +15,8 @@
 
 <div class="flex flex-row h-full">
 	<LeftNav>
+		<LeftNavLink href="/recipes/Glossary of New Mexican Foods" title="Glossary of New Mexican Foods">Glossary of New Mexican Foods</LeftNavLink>
+		<hr>
 		<LeftNavLink href="/recipes/Apetitos (Appetizers)" title="Apetitos (Appetizers)">Apetitos (Appetizers)</LeftNavLink>
 		<LeftNavLink href="/recipes/Bebidas (Beverages)" title="Bebidas (Beverages)">Bebidas (Beverages)</LeftNavLink>
 		<LeftNavLink href="/recipes/Caldos (Soups)" title="Caldos (Soups)">Caldos (Soups)</LeftNavLink>
@@ -23,7 +25,6 @@
 		<LeftNavLink href="/recipes/Ensaladas (Salads)" title="Ensaladas (Salads)">Ensaladas (Salads)</LeftNavLink>
 		<LeftNavLink href="/recipes/Especialidades Estilo Nuevo Mexicanos (New Mexican Specialties)" title="Especialidades Estilo Nuevo Mexicanos (New Mexican Specialties)">Especialidades Estilo Nuevo Mexicanos (New Mexican Specialties)</LeftNavLink>
 		<LeftNavLink href="/recipes/Galletas (Cookies)" title="Galletas (Cookies)">Galletas (Cookies)</LeftNavLink>
-		<LeftNavLink href="/recipes/Glossary of New Mexican Foods" title="Glossary of New Mexican Foods">Glossary of New Mexican Foods</LeftNavLink>
 		<LeftNavLink href="/recipes/Modo de Tostar Semillas y Nuezes del Pais (Roasting Native Nuts and Seeds)" title="Modo de Tostar Semillas y Nuezes del Pais (Roasting Native Nuts and Seeds)">Modo de Tostar Semillas y Nuezes del Pais (Roasting Native Nuts and Seeds)</LeftNavLink>
 		<LeftNavLink href="/recipes/Panes (Breads)" title="Panes (Breads)">Panes (Breads)</LeftNavLink>
 		<LeftNavLink href="/recipes/Platos Principales (Main Dishes)" title="Platos Principales (Main Dishes)">Platos Principales (Main Dishes)</LeftNavLink>
@@ -34,9 +35,6 @@
 		<div class="flex flex-row">
 			<div id="main-content" class="flex flex-col">
 				{@render children()}
-			</div>
-			<div id="right-content" class="hidden flex-col min-w-1/4 md:block">
-				
 			</div>
 		</div>
 	</main>
